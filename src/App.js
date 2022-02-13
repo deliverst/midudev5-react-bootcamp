951878f5861858bf2be18ddaa9407af3c2cb0c05
Javier Valles <deliverst@icloud.com>
2022-02-13 18:47:36 -0500

    d Altering data in server axios

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
 import './App.css'
 import {Note} from './Note'
 import {useState, useEffect} from 'react'
+// import axios from 'axios'
+import getAllNotes from './services/notes/getAllNotes'
+import createNote from './services/notes/createNote'
 
 const App = () => {
 
     const [notes, setNotes] = useState([])
-    const [newNote, setNewState] = useState('')
-    const [loader, setLoader] = useState(false)
+    const [newNote, setNewNote] = useState('')
+    // const [loader, setLoader] = useState(false)
 
     useEffect(() => {
-        setLoader(true)
-        setTimeout(() => {
-            fetch('https://jsonplaceholder.typicode.com/posts')
-                .then(res => res.json())
-                .then(json => {
-                    console.log(json)
-                    setNotes(json)
-                    setLoader(false)
-                })
-
-        }, 2000)
+        getAllNotes.then((notes) => {
+            console.log(notes)
+            setNotes(notes)
+        })
     }, [])
 
     const handleChange = (e) => {
-        const newNote = e.target.value
-        setNewState(e.target.value)
+        // const newNote = e.target.value
+        setNewNote(e.target.value)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const noteToAddToState = {
-            userId: notes.length + 1,
-            id: newNote,
-            title: newNote,
-            body: newNote
-        }
 
-        setNotes([...notes, noteToAddToState])
-        setNewState('')
+        const noteToAddToState = {
+            title: newNote,
+            body: newNote,
+            userId: 1
+        }
+        console.log(noteToAddToState)
+
+        createNote(noteToAddToState).then((newNote) => {
+            setNotes([...notes, newNote])
+        })
+
     }
 
     return (
         <>
             <ol>
                 <h1>Notes</h1>
-                {loader === true ? 'cargado...' : ''}
+                {/*{loader === true ? 'cargado...' : ''}*/}
                 {notes.map(note => <Note key={note.id} {...note}/>)}
             </ol>
 
